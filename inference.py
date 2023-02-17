7a9198397bbd1a09375d41a8a934b65f9359d1ca
bart <shyu0522@hanmail.net>
2023-02-17 11:30:49 +0900

    Update: inference code update

--- inference.py
+++ inference.py
@@ -1,25 +1,33 @@
+import os
 import torch
 import pytorch_lightning as pl
-from models.dense_model.model import CustomNet
-from models.rnn_model.model import LSTMModel
 from utils.compy import dataclass_to_namespace
+from utils.config_loader import load_config
 from arguments.inference_args import InferenceArguments
 from simple_parsing import ArgumentParser
+from models.transformer_ocr.model import TransformerOCR
+from models.transformer_ocr.datamodule import TransOCRDataModule
+
+
+def on_load_checkpoint(checkpoint):
+    state_dict = {k.partition("_forward_module.")[2]: checkpoint[k] for k in checkpoint.keys()}
+    checkpoint["state_dict"] = state_dict
+    return checkpoint
 
 
 def main(hparams):
     pl.seed_everything(hparams.seed)
+    device = torch.device("cuda")
+    os.makedirs("distributed_result", exist_ok=True)
+    model_config_dict = load_config(hparams.model_config)
+    datamodule = TransOCRDataModule(hparams)
+    model = TransformerOCR(model_config_dict)
+    checkpoint = torch.load(hparams.model_path, map_location=device)
+    checkpoint = on_load_checkpoint(checkpoint)
+    model.load_state_dict(checkpoint["state_dict"])
 
-    if hparams.model_select == "linear":
-        model = CustomNet.load_from_checkpoint(hparams.model_path, args=hparams)
-        features = torch.randn(1, 512)
-    else:
-        model = LSTMModel.load_from_checkpoint(hparams.model_path, args=hparams)
-        features = torch.randn(200, 1)
-    model.eval()
-    with torch.no_grad():
-        logits = model(features)
-    print(logits)
+    trainer = pl.Trainer.from_argparse_args(hparams)
+    trainer.predict(model, datamodule=datamodule, return_predictions=False)
 
 
 if __name__ == "__main__":
--- inference_deepspeed.py
+++ inference_deepspeed.py
@@ -2,13 +2,12 @@ import os
 import torch
 import pytorch_lightning as pl
 from utils.compy import dataclass_to_namespace
+from utils.config_loader import load_config
 from arguments.inference_args import InferenceArguments
 from simple_parsing import ArgumentParser
-from models.dense_model.model import CustomNet
-from models.dense_model.datamodule import CustomDataset
-from models.rnn_model.model import LSTMModel
+from models.transformer_ocr.model import TransformerOCR
+from models.transformer_ocr.datamodule import TransOCRDataModule
 from pytorch_lightning.callbacks import BasePredictionWriter
-from torch.utils.data import DataLoader, TensorDataset
 
 
 class CustomWriter(BasePredictionWriter):
@@ -23,15 +22,6 @@ class CustomWriter(BasePredictionWriter):
         torch.save(result_list, os.path.join(self.output_dir, f"predictions_{trainer.global_rank}.pt"))
 
 
-def read_json(fname):
-    from pathlib import Path
-    import json
-
-    fname = Path(fname)
-    with fname.open("rt") as handle:
-        return json.load(handle)
-
-
 def on_load_checkpoint(checkpoint):
     state_dict = {k.partition("_forward_module.")[2]: checkpoint[k] for k in checkpoint.keys()}
     checkpoint["state_dict"] = state_dict
@@ -42,34 +32,18 @@ def main(hparams):
     pl.seed_everything(hparams.seed)
     device = torch.device("cuda")
     os.makedirs("distributed_result", exist_ok=True)
-    if hparams.model_select == "linear":
-        model = CustomNet(hparams)
-        checkpoint = torch.load(hparams.model_path, map_location=device)
-        checkpoint = on_load_checkpoint(checkpoint)
-        model.load_state_dict(checkpoint["state_dict"])
-        features = torch.randn(10, 512)
-        temp_label = torch.randn(10, 1)
-        infer_datasets = CustomDataset(features, temp_label)
-        infer_loader = DataLoader(
-            dataset=infer_datasets, batch_size=hparams.per_device_test_batch_size, num_workers=4, pin_memory=True
-        )
-    else:
-        model = LSTMModel(hparams)
-        checkpoint = torch.load(hparams.model_path, map_location=device)
-        checkpoint = on_load_checkpoint(checkpoint)
-        model.load_state_dict(checkpoint["state_dict"])
-        features = torch.randn(200, 1)
-        temp_label = torch.randn(200, 1)
-        infer_datasets = TensorDataset(features, temp_label)
-        infer_loader = DataLoader(
-            dataset=infer_datasets, batch_size=hparams.per_device_test_batch_size, num_workers=4, pin_memory=True
-        )
+    model_config_dict = load_config(hparams.model_config)
+    datamodule = TransOCRDataModule(hparams)
+    model = TransformerOCR(model_config_dict)
+    checkpoint = torch.load(hparams.model_path, map_location=device)
+    checkpoint = on_load_checkpoint(checkpoint)
+    model.load_state_dict(checkpoint["state_dict"])
 
     pred_writer = CustomWriter(output_dir="distributed_result", write_interval="epoch")
     hparams.callbacks = [pred_writer]
     trainer = pl.Trainer.from_argparse_args(hparams)
 
-    trainer.predict(model, infer_loader, return_predictions=False)
+    trainer.predict(model, datamodule=datamodule, return_predictions=False)
 
 
 if __name__ == "__main__":
